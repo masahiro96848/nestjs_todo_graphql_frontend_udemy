e65348ddafafd71e2370cbbd9a5c7f0492f23486
masahiro96848 <feetwcpic9436@gmail.com>
2024-12-09 12:09:51 +0900

    ルーティングを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
 import { Signin } from './components/Signin'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Signup } from './components/Signup'
+import { Main } from './components/Main'
+import { NotFound } from './components/NotFound'
 
 function App() {
   return (
-    <div className="App">
-      <Signin />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
