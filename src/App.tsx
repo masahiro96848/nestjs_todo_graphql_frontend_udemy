27bdd11010cf75954c882f17bafc46d572277a64
masahiro96848 <feetwcpic9436@gmail.com>
2024-12-10 08:29:25 +0900

    apollo clientを作成

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,27 @@ import { Signup } from './components/Signup'
 import { Main } from './components/Main'
 import { NotFound } from './components/NotFound'
 import { GuestRoute, PrivateRoute } from './AuthRoute'
+import client from './apolloClient'
+import { ApolloProvider } from '@apollo/client'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/signin" element={<GuestRoute children={<Signin />} />} />
-        <Route path="/signup" element={<GuestRoute children={<Signup />} />} />
-        <Route path="/" element={<PrivateRoute children={<Main />} />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/signin"
+            element={<GuestRoute children={<Signin />} />}
+          />
+          <Route
+            path="/signup"
+            element={<GuestRoute children={<Signup />} />}
+          />
+          <Route path="/" element={<PrivateRoute children={<Main />} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
